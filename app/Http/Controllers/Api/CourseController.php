b863c9b10c0a4452d357e3dc144ff753ed2ccebb
Abdelghafour <achbaniabdelghafour8@gmail.com>
2026-03-18 12:27:02 +0000

    add crud course

---
+++ app/Http/Controllers/Api/CourseController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Services\CourseService;
+
+class CourseController extends Controller
+{
+    protected $courseService;
+
+    public function __construct(CourseService $courseService) {
+        $this->courseService = $courseService;
+        $this->middleware('auth:api');
+    }
+
+    public function index() {
+        return response()->json($this->courseService->listCourse());
+    }
+
+    public function show($id) {
+        return response()->json($this->courseService->getCourse($id));
+    }
+
+    public function store(Request $request) {
+        $data = $request->only(['title','description','price']);
+        return response()->json($this->courseService->createCourse($data));
+    }
+
+    public function update(Request $request , $id) {
+        $data = $request->only(['title','description','price']);
+        return response()->json($this->courseService->updateCourse($id , $data));
+    } 
+}
--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\CourseController;
 
 Route::post('/register',[AuthController::class,'register']);
 Route::post('/login',[AuthController::class,'login']);
 
 Route::middleware('auth:api')->group(function(){
     Route::post('/logout',[AuthController::class,'logout']);
+
+    Route::get('/courses',[CourseController::class,'index']);
+    Route::get('/courses/{id}',[CourseController::class,'show']);
+
+    Route::post('/courses',[CourseController::class,'store']);
+    Route::put('/courses/{id}',[CourseController::class,'update']);
+    Route::delete('/courses/{id}',[CourseController::class,'destroy']);
 });
