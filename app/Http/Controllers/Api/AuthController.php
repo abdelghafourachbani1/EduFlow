a91cf04aa3e27fe3eeeffc2d05d54a33550ebfd5
Abdelghafour <achbaniabdelghafour8@gmail.com>
2026-03-18 10:42:14 +0000

    add the authentification system

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -36,5 +36,26 @@ class AuthController extends Controller
 
     }
 
-    
+    public function login(Request $request) {
+        $credentials = $request->only('email','password');
+
+        if (!$token = auth()->attempt($credentials)) {
+            return response()->json([
+                'error' => 'Invalid credentials'
+            ],401);
+        }
+
+        return response()->json([
+                'token' => $token
+        ]);
+    }
+
+    public function logout()
+    {
+        auth()->logout();
+
+        return response()->json([
+            'message' => 'Successfully logged out'
+        ]);
+    }
 }
