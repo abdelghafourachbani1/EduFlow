5a5224048790c5997cb2aa316c0e41adc16a46f1
Abdelghafour <achbaniabdelghafour8@gmail.com>
2026-03-25 00:01:20 +0100

    add function pay controller

--- app/Http/Controllers/Api/PaymentController.php
+++ app/Http/Controllers/Api/PaymentController.php
@@ -3,9 +3,27 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Services\StripeService;
+use App\Models\Enrollment;
+use Illuminate\Support\Facades\Auth;
 
 class PaymentController extends Controller
 {
-    //
+
+    protected $stripe;
+
+    public function __construct(StripeService $stripe) {
+        $this->middlware('auth:api');
+        $this->stripe = $stripe;
+    }
+
+    public function pay($courseId) {
+        $session = $this->stripe->createCheckoutSession($courseId);
+        
+        return response()->json([
+            'url' => $session->url
+        ]);
+    }
+
+
 }
