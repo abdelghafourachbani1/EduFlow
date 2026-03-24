ca2d336f052b013d2b5cd7a64cd281b74d3c82c1
Abdelghafour <achbaniabdelghafour8@gmail.com>
2026-03-24 12:55:12 +0100

    file the recommendation controller

--- app/Http/Controllers/Api/RecommendationController.php
+++ app/Http/Controllers/Api/RecommendationController.php
@@ -3,9 +3,19 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Services\RecommendationService;
 use Illuminate\Http\Request;
 
 class RecommendationController extends Controller
 {
-    //
+    protected $service;
+
+    public function __construct(RecommendationService $service) {
+        $this->middleware('auth:api');
+        $this->service = $service;
+    }
+
+    public function index() {
+        return response()->json($this->service->getRecommendedCourses());
+    }
 }
