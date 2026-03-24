fca9d9b8f585cf229de2040abfc41732dd63193b
Abdelghafour <achbaniabdelghafour8@gmail.com>
2026-03-24 12:58:34 +0100

    add recommendation course

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CourseController;
+use App\Http\Controllers\Api\RecommendationController;
 
 Route::post('/register',[AuthController::class,'register']);
 Route::post('/login',[AuthController::class,'login']);
@@ -12,8 +13,10 @@ Route::middleware('auth:api')->group(function(){
 
     Route::get('/courses',[CourseController::class,'index']);
     Route::get('/courses/{id}',[CourseController::class,'show']);
-
     Route::post('/courses',[CourseController::class,'store']);
     Route::put('/courses/{id}',[CourseController::class,'update']);
     Route::delete('/courses/{id}',[CourseController::class,'destroy']);
+
+    Route::get('/recommended-courses',[RecommendationController::class,'index']);
+
 });
