f56680fae2d764f69417947d442a3c4cbbe2acf9
Abdelghafour <achbaniabdelghafour8@gmail.com>
2026-03-24 12:49:09 +0100

    get userREcommendation

--- app/Services/RecommendationService.php
+++ app/Services/RecommendationService.php
@@ -4,3 +4,16 @@ namespace App\Services;
 
 use Illuminate\Support\Facades\Auth;
 use App\Models\Course;
+
+class RecommendationService {
+
+    public function getRecommendedCourses() {
+        $user = Auth::user();
+        $interestIds = $user->interests->pluck('id');
+
+        return Course::whereHas('interests' , function($query) use ($interestIds) {
+            $query->whereIn('intrests.id' , $interestIds);
+        })->get(); 
+    }
+    
+}
