eb14fb6f32950330e2384447ce39ba456604e7b8
Abdelghafour <achbaniabdelghafour8@gmail.com>
2026-03-30 12:36:32 +0100

    add the register page

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -4,3 +4,16 @@
 
 <h2>Register</h2>
 
+<form id="registerForm">
+    <input type="text" id="name" placeholder="Name"><br>
+    <input type="email" id="email" placeholder="Email"><br>
+    <input type="password" id="password" placeholder="Password"><br>
+
+    <select  id="role">
+        <option value="student">student</option>
+        <option value="teacher">teacher</option>
+    </select><br>
+
+    <button type="submit">Register</button>
+
+</form>
