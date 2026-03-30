455035a17b30e8fb170ba35030d12e56a41aebfd
Abdelghafour <achbaniabdelghafour8@gmail.com>
2026-03-30 12:12:28 +0100

    Create Blade Layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,2 +1,15 @@
 <!DOCTYPE html>
+<head>
+    <title>EduFlow</title>
+</head>
+<body>
+    <nav>
+        <a href="/">Home</a>
+        <a href="/login">Login</a>
+        <a href="/register">Register</a>
+    </nav>
+    <hr>
+    @yield('content')
 
+    <script src="{{asset(js/app.js)}}"></script>
+</body>
