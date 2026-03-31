f119265d2da62734eae2967df71b8be4b0a38587
Abdelghafour <achbaniabdelghafour8@gmail.com>
2026-03-31 15:19:02 +0100

    add routes in web.php

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,6 @@ Route::get('/', function () {
 Route::view('/', 'home');
 Route::view('/login', 'login');
 Route::view('/register', 'register');
+
+Route::view('/courses','courses');
+Route::view('/courses/{id}','course-details');
