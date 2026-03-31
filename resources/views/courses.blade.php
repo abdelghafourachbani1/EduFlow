722639220c2290bca10a13543b3b400a4ffe85fc
Abdelghafour <achbaniabdelghafour8@gmail.com>
2026-03-31 22:35:02 +0100

    teacher can create Course

--- resources/views/courses.blade.php
+++ resources/views/courses.blade.php
@@ -8,4 +8,14 @@
 
 <div id="courseContainer"></div>
 
+<h3>Create Course</h3>
+
+<form id="createCourseForm">
+    <input placeholder="Totle" id ="Title"><br>
+    <input placeholder="description" id="Description"><br>
+    <input placeholder="Price" id ="Price">
+
+    <button type="submit">create</button>
+</form>
+
 @endsection
