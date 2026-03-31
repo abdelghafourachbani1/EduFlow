0100d5028e0f6d733d2090983b0f6ba4ada13171
Abdelghafour <achbaniabdelghafour8@gmail.com>
2026-03-31 15:23:47 +0100

    can list the courses

---
+++ resources/views/courses.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h2>Courses</h2>
+
+<input type="text" id="search" placeholder="Search courses...">
+
+<div id="courseContainer"></div>
+
+@endsection
